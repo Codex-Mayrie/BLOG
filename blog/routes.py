74175c2a77f8bc29c5c4b2ca275c867fd889004e
shee1998 <marywanjikumburu1@gmail.com>
2019-12-03 10:16:09 +0300

    create a url_for reset request when one forgets the password

--- blog/routes.py
+++ blog/routes.py
@@ -1,10 +1,11 @@
 import os
 import secrets
 from flask import render_template, url_for, flash, redirect, request, abort
-from blog import app, db, bcrypt
+from blog import app, db, bcrypt, mail
 from blog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm, RequestResetForm, ResetPasswordForm
 from blog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
+from flask_mail import Message
 
 
 @app.route("/")
